c43fdb189ca646f3d697ad223f92fe24dbdfc5a5
Thanos Paravantis <p16112@students.cs.unipi.gr>
2018-07-04 18:19:11 +0300

    Generate unique P matrix

--- fanoShannon.py
+++ fanoShannon.py
@@ -177,6 +177,11 @@ def linear_encode(width, height, rgb_code, error=0, n=7, k=4):
 
     I = np.eye(k, dtype=int)
     P = np.random.randint(low=0, high=2, size=(k, n-k), dtype=int)
+
+    while len(np.unique(P, axis=0)) < P.shape[0]:
+        print("Generating new P...")
+        P = np.random.randint(low=0, high=2, size=(k, n - k), dtype=int)
+
     # TODO: p.153====================================
     # P= [[0,1,1],[1,0,1],[1,1,0]]
     # ===============================================
